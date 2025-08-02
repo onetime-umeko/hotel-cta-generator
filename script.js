8f16321d7cf6f2e2124ec3965f8b49283b0f55a5
onetime-umeko <fufunotabi1202@gmail.com>
2025-08-02 16:42:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function generateHTML() {
 
   <p class="coupon-guide-link">
     ▶ 最新クーポン情報は
-    <a href="/category/deals/" target="_blank">こちらのお得情報ページ</a>にまとめています。
+    <a href="https://fufunotabi.com/otokumatome" target="_blank">こちらのお得情報ページ</a>にまとめています。
   </p>
 </div>
 `;
